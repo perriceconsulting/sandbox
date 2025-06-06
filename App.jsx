706a9c865abf32952d500f368bc239ca5a784a68
perriceconsulting <perriceconsulting@gmail.com>
2025-06-06 14:48:52 -0400

    Add Tailwind CSS setup and styling

--- App.jsx
+++ App.jsx
@@ -3,9 +3,9 @@ import Sidebar from './Sidebar';
 import Playlist from './Playlist';
 
 const App = () => (
-  <div className="app">
+  <div className="flex h-screen">
     <Sidebar />
-    <main className="content">
+    <main className="flex-1 p-4 bg-gray-100">
       <Playlist />
     </main>
   </div>
